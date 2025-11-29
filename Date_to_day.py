88d15c7515fc6dbb380aa7bf30d979f30d14f44f
Yashneel chauhan <yashneelchauhan69@gmail.com>
2025-11-29 12:18:02 +0530

    Add files via upload

---
+++ Date_to_day.py
@@ -0,0 +1,12 @@
+import datetime
+
+year = int(input("Enter your year: "))
+month = int(input("Enter your month: "))
+day = int(input("Enter your day: "))
+
+birth_date = datetime.date(year, month, day)
+weekends = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+day_number = birth_date.weekday()
+day_name = weekends[day_number]
+
+print(day_name)
